28c8ec9fc28c123aead3423995e73f3e35a200e0
Taylor McKeel <josephmckeel1993@gmail.com>
2020-04-25 09:26:15 -0400

    saturday 4/25

--- questions/01_multiply/multiply.js
+++ questions/01_multiply/multiply.js
@@ -1,5 +1,11 @@
 // This stretch requires you to write your own test cases
 
-const multiply = (num1, num2) => {};
+const multiply = (num1, num2) => {
+  if (typeof num1 !== "number" || typeof num2 !== "number") {
+    throw "error";
+  }
+
+  return num1 * num2;
+};
 
 module.exports = { multiply };
