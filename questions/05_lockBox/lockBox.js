afbbae55bffcf12626089e8ab80ea1d43567b42f
Taylor McKeel <josephmckeel1993@gmail.com>
2020-05-04 18:56:59 -0400

    lockbox

--- questions/05_lockBox/lockBox.js
+++ questions/05_lockBox/lockBox.js
@@ -1,5 +1,28 @@
-const lockBox = () => {
-  // write code here
+const lockBox = (code, msg) => {
+  let final = code;
+  let message = msg;
+  if (typeof msg !== "string") {
+    throw new Error(message);
+  }
+  return {
+    modCode: function modCode(code, newCode) {
+      final = newCode;
+      return final;
+    },
+
+    modMessage: function modMessage(code, newMsg) {
+      message = newMsg;
+      return message;
+    },
+
+    revealMessage: function revealMessage(test) {
+      if (code !== test) {
+        throw new Error(message);
+      } else {
+        return message;
+      }
+    },
+  };
 };
 
 module.exports = { lockBox };
