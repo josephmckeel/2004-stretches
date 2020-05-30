e8d58aa1180846d3cab4949bd4fcb9a81346435c
Taylor McKeel <josephmckeel1993@gmail.com>
2020-05-30 12:10:44 -0500

    last spec not working

--- questions/18_arrayexchange/arrayexchange.js
+++ questions/18_arrayexchange/arrayexchange.js
@@ -1,40 +1,26 @@
 //arrayexchange should exchange the elements of two arrays in-place in a way that their new content is also reversed.
 //restricted use: reverse()
 
-// const arrayexchange = (arr1, arr2) => {
-//   if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
-//     throw new Error("nope");
-//   }
-//   let new1 = [];
-//   let new2 = [];
-
-//   arr1.map((item) => {
-//     new1.unshift(item);
-//   });
-//   arr2.map((item) => {
-//     new2.unshift(item);
-//   });
-
-//   arr1 = [...new1];
-//   arr2 = [...new2];
-// }; need to swap the two arrays
-
 const arrayexchange = (arr1, arr2) => {
   if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
     throw new Error("nope");
   }
-  let new1 = [];
-  let new2 = [];
+
+  let num1 = arr1.length;
+  let num2 = arr2.length;
 
   for (let i = 0; i < arr1.length; i++) {
-    new1.unshift(arr1[i]);
-    arr1.shift();
+    arr2.unshift(arr1[i]);
   }
-  arr2.forEach((item) => {
-    console.log(item);
-    new2.unshift(item);
-    arr2.shift();
-  });
+  for (let i = 0; i < arr2.length; i++) {
+    if (arr1.includes(arr2[i])) {
+      continue;
+    } else {
+      arr1.unshift(arr2[i]);
+    }
+  }
+  arr2.splice(num1, num2);
+  arr1.splice(num2, num1);
 };
 
 module.exports = { arrayexchange };
